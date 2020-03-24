d41cb2506db67cbd0234333c2f0593a1857bf7d5
Jürgen Baltres <Baltrescu@Jurgens-MBP.fritz.box>
2020-03-24 10:26:42 +0100

    try to refactor a image slider in react

--- brasov-app/src/components/Slider.js
+++ brasov-app/src/components/Slider.js
@@ -1,12 +1,26 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 
-const Quader = styled.span`
-width: 80px;
+
+const QuaderBox = styled.div`
+display: flex;
+justify-content: center
+`;
+
+const Quader = styled.img`
+width: 50%;
 height: 90px;
 background: red;
+display: flex;
+align-items: center;
+justify-content: center;
+margin: 10px
 `;
 
 export default function Slider() {
-  return <Quader>Zimmer</Quader>;
+  return (
+    <QuaderBox> 
+        <Quader src="https://a0.muscache.com/im/pictures/62c9cb76-c15d-44a2-bea2-52d36a82dbc6.jpg?aki_policy=xx_large"></Quader>
+    </QuaderBox>   
+  )
 }
--- brasov-app/src/pages/Zimmer.js
+++ brasov-app/src/pages/Zimmer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components"
+import PlaySlider from "../components/SliderPlayGround";
 
 const HomeText = styled.span`
 font-size: 80px
 `;
 
 export default function Home() {
-  return <HomeText>Zimmer</HomeText>;
+  return <PlaySlider></PlaySlider>;
 }
 
