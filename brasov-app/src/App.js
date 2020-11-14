b4674e8b30c5de3619ea8cd80c5149645dc92299
Jürgen Baltres <jbaltres@Jurgens-MBP.fritz.box>
2020-11-14 12:49:45 +0100

    Changed text that it´s visibile on all devices and delete the CRA styles

--- brasov-app/src/App.js
+++ brasov-app/src/App.js
@@ -1,22 +1,16 @@
-import React from 'react';
-import styled from 'styled-components'
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import Preise from "./pages/Preise";
 import Zimmer from "./pages/Zimmer";
-import MainImage from './components/MainImage';
-import Header from './components/Header';
+import MainImage from "./components/MainImage";
+import Header from "./components/Header";
 
 const Background = styled.div`
 background: papayawhip;
-height: 100vh;
--webkit-animation: color-change 2s infinite;
-    -moz-animation: color-change 2s infinite;
-    -o-animation: color-change 2s infinite;
-    -ms-animation: color-change 2s infinite;
-    animation: color-change 2s infinite;
 }
 
 @-webkit-keyframes color-change {
@@ -36,52 +30,47 @@ height: 100vh;
 
 `;
 
-
 const ToggleButton = styled.span`
-align-items: center;
-padding: 10px;
-margin-top: 3px;
-z-index: 400;
-justify-content: space-around;
-box-shadow: -6px -9px 22px hsla(300,15%,25%,0.8) inset
+  align-items: center;
+  padding: 10px;
+  margin-top: 3px;
+  z-index: 400;
+  justify-content: space-around;
+  box-shadow: -6px -9px 22px hsla(300, 15%, 25%, 0.8) inset;
 
-@media (max-width: 768px) {
-  flex-direction: column;
-  align-items: flex-start;
-};
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+  } ;
 `;
 
 const MenuContainer = styled.div`
-display:flex;
+  display: flex;
 `;
 
 function App() {
-
-    const [isToggled, setToggled] = React.useState(true);
-   
+  const [isToggled, setToggled] = React.useState(true);
 
   return (
-<Router>
-    <Header/>
-    <MainImage></MainImage>
-     <MenuContainer>
-    <ToggleButton onClick={ () => setToggled(!isToggled)}>
+    <Router>
+      <Header />
+      <MainImage></MainImage>
+      <MenuContainer>
+        <ToggleButton onClick={() => setToggled(!isToggled)}>
           <h3>Menu →</h3>
-    </ToggleButton>
-    <NavBar handleToggle={isToggled}></NavBar>  
-    </MenuContainer>    
-    <Background>
-    <Route exact path="/">
-    <Redirect to="/home" />
-    </Route>   
-    <Route path="/home" component={Home} />
-    <Route path="/preise" component={Preise} />
-    <Route path="/zimmer" component={Zimmer} />
-    </Background>
-</Router>
+        </ToggleButton>
+        <NavBar handleToggle={isToggled}></NavBar>
+      </MenuContainer>
+      <Background>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home" component={Home} />
+        <Route path="/preise" component={Preise} />
+        <Route path="/zimmer" component={Zimmer} />
+      </Background>
+    </Router>
   );
 }
 
 export default App;
-
-        
--- brasov-app/src/pages/Home.js
+++ brasov-app/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 
 const CardWrapper = styled.div`
   width: 90%;
@@ -7,38 +7,38 @@ const CardWrapper = styled.div`
 `;
 
 const WelcomeText = styled.div`
-font-family: cursive;
-font-size: 20px;
-background-image:url("https://ioanaphotos.files.wordpress.com/2010/09/p1070396.jpg?w=758&h=&zoom=2");
-width: 90%;
-margin: 15px auto; 
--webkit-background-clip: text;
--webkit-text-fill-color: transparent;
-`;
-
-const HomePicture = styled.img`
-width: 100%;
-margin: 25px 0 5px 0;
+  font-family: cursive;
+  font-size: 20px;
+  width: 90%;
+  margin: 15px auto;
+  color: rgba(41, 49, 51, 0.8);
 `;
 
 const Headline = styled.h2`
-font-size: 2em;
+  font-size: 2em;
 `;
 
 export default function Home() {
   return (
     <CardWrapper>
       <WelcomeText>
-        <Headline>Willkommen in Siebenbürgen.</Headline> 
-        <p/>
-        Wir begrüßen Sie in unseren Ferienwohnungen und Fremdenzimmern im Herzen Rumäniens. <br/>
-        Aus unserer Unterkunft gelangen Sie schnell ins Stadtzentrum von Kronstadt/Braşov, wo Sie zahlreiche Einkaufsmöglichkeiten und Gastronomien mit landestypischen Spezialitäten erwarten. <br/>
-        Von Kronstadt aus gelangen Sie schnell zu den höchsten Spitzen der Karpaten, die waldreichste Gegend Europas, für welche das Land bekannt ist. <br/><br/> 
-        Ein Super Platz für alle Wanderfreunde.<br/> <br/> 
-        Im Winter läd sie unter anderem das anliegende Skigebiet zum Skifahren, oder zu einem wärmenden Glühschnaps, ein. Mehr über Braşov lernen Sie hier.      
-        </WelcomeText>
-      <HomePicture src="https://ioanaphotos.files.wordpress.com/2010/09/p1070396.jpg?w=758&h=&zoom=2"></HomePicture>
+        <Headline>Willkommen in Siebenbürgen.</Headline>
+        <p />
+        Wir begrüßen Sie in unseren Ferienwohnungen und Fremdenzimmern im Herzen
+        Rumäniens. <br />
+        Aus unserer Unterkunft gelangen Sie schnell ins Stadtzentrum von
+        Kronstadt/Braşov, wo Sie zahlreiche Einkaufsmöglichkeiten und
+        Gastronomien mit landestypischen Spezialitäten erwarten. <br />
+        Von Kronstadt aus gelangen Sie schnell zu den höchsten Spitzen der
+        Karpaten, die waldreichste Gegend Europas, für welche das Land bekannt
+        ist. <br />
+        <br />
+        Ein Super Platz für alle Wanderfreunde.
+        <br /> <br />
+        Im Winter läd sie unter anderem das anliegende Skigebiet zum Skifahren,
+        oder zu einem wärmenden Glühschnaps, ein. Mehr über Braşov lernen Sie
+        hier.
+      </WelcomeText>
     </CardWrapper>
-  )
+  );
 }
-
