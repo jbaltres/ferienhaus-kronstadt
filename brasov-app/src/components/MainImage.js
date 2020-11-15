e5dc636db427d5ea2996d5ea854ef72ac70c2766
Jürgen Baltres <jbaltres@Jurgens-MBP.fritz.box>
2020-11-15 12:32:03 +0100

    changed the Homepage Image and restructure

--- brasov-app/src/components/MainImage.js
+++ brasov-app/src/components/MainImage.js
@@ -2,50 +2,54 @@ import React from "react";
 import styled from "styled-components";
 
 const HeaderContainer = styled.div`
-width:100%;
-height:400px;
-display:flex; 
-justify-content: space-around;
-@media (max-width: 768px) {
-  margin: 0px;
-};
-margin: 10px 0px ;
+  width: 100%;
+  height: 400px;
+  display: flex;
+  justify-content: space-around;
+  @media (max-width: 768px) {
+    margin: 0px;
+  }
+  @media (max-width: 350px) {
+    margin-top: 55px;
+  }
+  margin: 10px 0px;
 `;
 
 const HeadImage = styled.img`
-float:left;
- height:100%;
- object-fit:cover;
+  float: left;
+  height: 100%;
+  object-fit: cover;
   width: 45%;
   @media (max-width: 768px) {
     width: 100%;
-  };
+  }
   display: block;
 `;
 
 const HeadImage2 = styled.img`
-float:left;
- height:100%;
- object-fit:cover;
+  float: left;
+  height: 100%;
+  object-fit: cover;
   width: 45%;
   @media (max-width: 768px) {
     width: 0%;
-  };
+  }
   display: block;
 `;
 
-
 function MainImage() {
-    return (
-        <HeaderContainer>
-          <HeadImage src="https://urlaub-in-rumänien.de/wp-content/uploads/2017/02/3240px-Brasov_Piata_Sfatului.jpg" alt="Pic1"/> 
-          <HeadImage2 src="https://s.inyourpocket.com/img/figure/2015-04/dreamstime_xxl_18728583.jpg" alt="Pic2"/> 
-        </HeaderContainer>
-    );
-  }
-
-
-
+  return (
+    <HeaderContainer>
+      <HeadImage
+        src="https://urlaub-in-rumänien.de/wp-content/uploads/2017/02/3240px-Brasov_Piata_Sfatului.jpg"
+        alt="Pic1"
+      />
+      <HeadImage2
+        src="https://s.inyourpocket.com/img/figure/2015-04/dreamstime_xxl_18728583.jpg"
+        alt="Pic2"
+      />
+    </HeaderContainer>
+  );
+}
 
 export default MainImage;
-
