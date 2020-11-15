fe56aabe1f0883c22911e7679f21b3ea404e37f0
Jürgen Baltres <jbaltres@Jurgens-MBP.fritz.box>
2020-11-15 12:32:59 +0100

    show the User the current language

--- brasov-app/src/components/Header.js
+++ brasov-app/src/components/Header.js
@@ -1,37 +1,60 @@
 import React from "react";
 import styled from "styled-components";
 
-const Title = styled.header`
-  display: flex;
-  align-items: center;
-  padding: 10px;
-  background-color: rgb(0, 0, 0);
-  margin: 0;
-  border-width: 2px;
-  border-style: solid;
-  border-image: linear-gradient(to right, rgba(37, 206, 197, 1), black) 50 15%;
-  z-index: 400;
-  justify-content: space-around;
-`;
+function Header(props) {
+  const Title = styled.header`
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    background-color: rgb(0, 0, 0);
+    margin: 0;
+    border-width: 2px;
+    border-style: solid;
+    border-image: linear-gradient(to right, rgba(37, 206, 197, 1), black) 50 15%;
+    z-index: 2000;
+    justify-content: space-around;
+    width: 100%;
+    position: fixed;
+  `;
 
-const Headline = styled.span`
-  font-size: 25px;
-  color: rgb(0, 195, 238);
-  
-  flex-grow: 1;
-  text-align: center;
-  font-family: -apple-system, BShitMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-`;
+  const Headline = styled.span`
+    font-size: 25px;
+    color: rgb(0, 195, 238);
 
+    flex-grow: 1;
+    text-align: center;
+    font-family: -apple-system, BShitMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  `;
 
-function Header() {
+  const CurrentFlag = styled.img`
+    display: flex;
+    width: 35px;
+    height: 35px;
+    margin-right: 8%;
+    margin-left: 7px;
+  `;
+
   return (
     <Title>
-        <Headline>Ferienwohnung Kronstadt</Headline>
+      <Headline>Ferienwohnung Kronstadt</Headline>
+      {props.language === "deutsch" ? (
+        <CurrentFlag src="https://cdn.countryflags.com/thumbs/germany/flag-round-250.png"></CurrentFlag>
+      ) : props.language === "englisch" ? (
+        <CurrentFlag src="https://cdn.countryflags.com/thumbs/united-states-of-america/flag-round-250.png"></CurrentFlag>
+      ) : props.language === "rumänisch" ? (
+        <CurrentFlag src="https://cdn.countryflags.com/thumbs/romania/flag-button-round-250.png"></CurrentFlag>
+      ) : props.language === "spanisch" ? (
+        <CurrentFlag src="https://cdn.countryflags.com/thumbs/spain/flag-button-round-250.png"></CurrentFlag>
+      ) : props.language === "ungarisch" ? (
+        <CurrentFlag src="https://cdn.countryflags.com/thumbs/hungary/flag-button-round-250.png"></CurrentFlag>
+      ) : props.language === "französisch" ? (
+        <CurrentFlag src="https://cdn.countryflags.com/thumbs/france/flag-3d-round-250.png"></CurrentFlag>
+      ) : (
+        <CurrentFlag src="https://cdn.countryflags.com/thumbs/israel/flag-3d-round-250.png"></CurrentFlag>
+      )}
     </Title>
   );
 }
 
 export default Header;
-
