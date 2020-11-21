e24cdd5e3bc76766b1ad6f1980fb381daecd62dd
lkastenmeier <l.kastenmeier@web.de>
2020-11-21 20:04:35 +0100

    create component for apartment to avoid duplicate code

--- brasov-app/src/components/Wohnungen.js
+++ brasov-app/src/components/Wohnungen.js
@@ -1,68 +1,247 @@
 import React from "react";
 import styled from "styled-components";
-
+import Slider from "../components/Slider";
+import AlertBox from "../components/AlertBox";
 const Wohnungswrapper = styled.div`
-display:flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-@media (max-width: 768px) {
+  @media (max-width: 768px) {
     width: 100%;
-  };
-
+  } ;
 `;
 
 const Image = styled.img`
-width: 90%;
-height: 200px;
-border-radius: 30%;
-margin: 0px auto;
+  width: 90%;
+  height: 200px;
+  border-radius: 30%;
+  margin: 0px auto;
 `;
 
 const Table = styled.table`
-border-width: 2px;
-border-style: solid;
-border-image: linear-gradient(to right, rgba(37, 206, 197, 1), black) 50 15%;`;
+  border-width: 2px;
+  border-style: solid;
+  border-image: linear-gradient(to right, rgba(37, 206, 197, 1), black) 50 15%;
+`;
 
 const TableLines = styled.tr`
-border-width: 2px;
-border-style: solid;
-border-image: linear-gradient(to right, rgba(37, 206, 197, 1), black) 50 15%;`;
-
+  border-width: 2px;
+  border-style: solid;
+  border-image: linear-gradient(to right, rgba(37, 206, 197, 1), black) 50 15%;
+`;
 
 function Wohnungen() {
-    return (
-        <Wohnungswrapper>
-            <Image src="https://urlaub-in-rumänien.de/wp-content/uploads/2017/02/3240px-Brasov_Piata_Sfatului.jpg"></Image>
-            <Table>
-                <TableLines>Appartment I</TableLines>
-                <TableLines>
-                    <td>Gäste</td>
-                    <td>2-4</td>
-                </TableLines>
-                <TableLines>
-                  <td>Betten</td>
-                  <td>2</td>
-                </TableLines>
-                <TableLines>
-                  <td>Bad</td>
-                  <td>√</td>
-                </TableLines>
-                <TableLines>
-                  <td>TV</td>
-                  <td>2</td>
-                </TableLines>
-                <TableLines>
-                  <td>WIFI</td>
-                  <td>Ja</td>
-                </TableLines>
-                <TableLines>
-                  <td>Weiteres</td>
-                  <td>Große Küche auf Wunsch mit Mikrowelle, Föhn, Handtücher und Bettwäsche vor Ort, Ansprechpartner vor Ort</td>  
-                </TableLines>
-            </Table>
-        </Wohnungswrapper>
-    );
+  return (
+    <Wohnungswrapper>
+      <Image src="https://urlaub-in-rumänien.de/wp-content/uploads/2017/02/3240px-Brasov_Piata_Sfatului.jpg"></Image>
+      <Table>
+        <TableLines>Appartment I</TableLines>
+        <TableLines>
+          <td>Gäste</td>
+          <td>2-4</td>
+        </TableLines>
+        <TableLines>
+          <td>Betten</td>
+          <td>2</td>
+        </TableLines>
+        <TableLines>
+          <td>Bad</td>
+          <td>√</td>
+        </TableLines>
+        <TableLines>
+          <td>TV</td>
+          <td>2</td>
+        </TableLines>
+        <TableLines>
+          <td>WIFI</td>
+          <td>Ja</td>
+        </TableLines>
+        <TableLines>
+          <td>Weiteres</td>
+          <td>
+            Große Küche auf Wunsch mit Mikrowelle, Föhn, Handtücher und
+            Bettwäsche vor Ort, Ansprechpartner vor Ort
+          </td>
+        </TableLines>
+      </Table>
+    </Wohnungswrapper>
+  );
+}
+
+function Apartment({
+  languages,
+  language,
+  pic,
+  title,
+  pax,
+  rooms,
+  phone,
+  link,
+  colors,
+  buchbar,
+  price,
+}) {
+  const [show, setShow] = React.useState(true);
+  const [clickable, setClickable] = React.useState(true);
+
+  const handleDisable = () =>
+    setTimeout(function () {
+      setClickable(!clickable);
+    }, 3000);
+
+  function handleonClick() {
+    handleDisable();
+    setShow(!show);
+  }
+  function handleonClick2() {
+    setClickable(true);
+    setShow(!show);
   }
 
+  const Pricing = styled.ul`
+    list-style: none;
+    margin: 2em;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    border-top-left-radius: 30px;
+    border-bottom-right-radius: 50px;
+    color: white;
+    background: ${(props) => props.bgcolor || "yellow"};
+    position: relative;
+    width: 350px; /* width of each table */
+    margin-right: 30px; /* spacing between tables */
+    margin-bottom: 1em;
+    transition: all 0.5s;
 
-export default Wohnungen;
+    :hover {
+      transition: all 0.5s;
+
+      box-shadow: 0 0 10px gray;
+    }
+  `;
+  const Phone = styled.div`
+    display: none;
+    background-color: rgba(41, 49, 51, 0.6);
+    width: fit-content;
+    border-radius: 10px;
+    padding: 3px 10px;
+  `;
+
+  const ListItem = styled.li`
+    margin: 10px 20px;
+    position: relative;
+    &:hover ${Phone} {
+      display: flex;
+    }
+  `;
+
+  const LiTitle = styled.li`
+    font-size: 150%;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 20px;
+    text-transform: uppercase;
+    border-bottom: 5px solid white;
+  `;
+
+  const LinkButton = styled.button`
+    background: #31a3cb;
+    text-decoration: none;
+    padding: 10px;
+    display: inline-block;
+    max-width: 150px;
+    margin: 10px auto;
+    border-radius: 5px;
+    text-transform: uppercase;
+    border: none;
+
+    :hover {
+      transform: scale(0.9);
+      box-shadow: 0 4px 8px 0 rgba(0, 195, 238, 1),
+        0 6px 20px 0 rgba(0, 195, 238, 1);
+    }
+  `;
+
+  const Anker = styled.a`
+    text-decoration: none;
+    color: white;
+  `;
+
+  const AlertWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  const Button = styled.button`
+    display: ${!show ? "flex" : "none"};
+    font-size: 1em;
+    position: fixed;
+    bottom: 23%;
+    margin-left: auto;
+    margin-right: auto;
+    z-index: 1999999999999;
+    background: ${!clickable
+      ? "linear-gradient(45deg,#1ed0bb,#ffad5c)"
+      : "grey"};
+    color: #000;
+    padding: 10px;
+    border-radius: 6px;
+    cursor: pointer;
+    @media (min-width: 1256px) {
+      right: 50%;
+    }
+  `;
+  return (
+    <Pricing bgcolor={colors}>
+      <Slider pic1={pic}></Slider>
+      <LiTitle>{title}</LiTitle>
+      <ListItem>
+        <b>{languages[language].Feld1}</b> {pax}
+      </ListItem>
+      <ListItem>
+        <b>{languages[language].Feld3}</b> {rooms}
+      </ListItem>
+      <ListItem>
+        <b>{languages[language].Feld5}</b> {languages[language].Feld6}
+      </ListItem>
+      <ListItem>
+        <b>{languages[language].Feld7}</b> {languages[language].Feld8}
+      </ListItem>
+      <ListItem>
+        <b>Netflix:</b>
+        <span role="img" aria-label="Yes">
+          ✔️
+        </span>{" "}
+      </ListItem>
+      <ListItem>
+        <b>{languages[language].Feld11}</b> {languages[language][price]}
+      </ListItem>
+      <ListItem>
+        <b>{languages[language].Feld13}</b>
+
+        <Phone>{phone}</Phone>
+      </ListItem>
+      {buchbar ? (
+        <LinkButton>
+          <Anker href={link}>{languages[language].AirBnbButton}}</Anker>
+        </LinkButton>
+      ) : (
+        <>
+          <LinkButton onClick={handleonClick}>
+            <Anker>{languages[language].AirBnbButton}</Anker>
+          </LinkButton>
+          <AlertWrapper>
+            <AlertBox language={language} show={!show}></AlertBox>
+            <Button onClick={handleonClick2} disabled={clickable}>
+              {languages[language].ZurückButton}
+            </Button>
+          </AlertWrapper>
+        </>
+      )}
+    </Pricing>
+  );
+}
+
+export default Apartment;
--- brasov-app/src/languages/api.js
+++ brasov-app/src/languages/api.js
@@ -1,6 +1,6 @@
 export const deutsch = {
   Feld1: "Personen:",
-  Feld2: 2 - 4,
+  Feld2: "2-4",
   Feld3: "Zimmer:",
   Feld4: 2,
   Feld5: "Küchenzeile:",
@@ -14,7 +14,8 @@ export const deutsch = {
   Feld12: "ab 40€ / Nacht",
   Feld12Garsoniera: "ab 22€ / Nacht",
   Feld12APPII: "ab 35€ / Nacht",
-  Feld13:"Spezielle Angebote für Langzeitmieter und Vergünstigungen auf Anfrage.",
+  Feld13:
+    "Spezielle Angebote für Langzeitmieter und Vergünstigungen auf Anfrage.",
   AirBnbButton: "Hier Verfügbarkeit überprüfen und buchen",
   TextHeadline: "Willkommen in Siebenbürgen.",
   Text1:
@@ -29,14 +30,14 @@ export const deutsch = {
   HomeLink: "Home",
   ZimmerLink: "Zimmer",
   GallerieLink: "Gallerie",
-  AlertText1:"Dieses Zimmer ist online momentan nicht verfügbar.",
-  AlertText2:"Anfragen & Zimmerbuchungen unter:",
-  ZurückButton:"Zurück"
+  AlertText1: "Dieses Zimmer ist online momentan nicht verfügbar.",
+  AlertText2: "Anfragen & Zimmerbuchungen unter:",
+  ZurückButton: "Zurück",
 };
 
 export const englisch = {
   Feld1: "Persons:",
-  Feld2: 2 - 4,
+  Feld2: "2-4",
   Feld3: "Rooms:",
   Feld4: 2,
   Feld5: "Kitchen:",
@@ -50,7 +51,7 @@ export const englisch = {
   Feld12: "from 40€ / night",
   Feld12Garsoniera: "from 22€ / night",
   Feld12APPII: "from 35€ / night",
-  Feld13:"Special offers for long-term rent and discounts on request.",
+  Feld13: "Special offers for long-term rent and discounts on request.",
   AirBnbButton: "Check availability and book here",
   TextHeadline: "Welcome to Transylvania.",
   Text1:
@@ -65,14 +66,14 @@ export const englisch = {
   HomeLink: "Home",
   ZimmerLink: "Rooms",
   GallerieLink: "Gallery",
-  AlertText1:"This room is currently unavailable online.",
-  AlertText2:"Please call the number for inquiries & room bookings:",
-  ZurückButton:"Back"
+  AlertText1: "This room is currently unavailable online.",
+  AlertText2: "Please call the number for inquiries & room bookings:",
+  ZurückButton: "Back",
 };
 
 export const rumänisch = {
   Feld1: "Persoane:",
-  Feld2: 2 - 4,
+  Feld2: "2-4",
   Feld3: "Cameră:",
   Feld4: 2,
   Feld5: "Bucătărie:",
@@ -86,7 +87,8 @@ export const rumänisch = {
   Feld12: "de la 40 € / noapte",
   Feld12Garsoniera: "de la 22 € / noapte",
   Feld12APPII: "de la 35 € / noapte",
-  Feld13:"Oferte speciale pentru chiriașii pe termen lung și reduceri la cerere.",
+  Feld13:
+    "Oferte speciale pentru chiriașii pe termen lung și reduceri la cerere.",
   AirBnbButton: "Verificați disponibilitatea și rezervați aici",
   TextHeadline: "Bine ați venit la Brașov.",
   Text1: "Vă așteptăm în apartamentele și camerele noastre din inima României.",
@@ -100,14 +102,14 @@ export const rumänisch = {
   HomeLink: "Home",
   ZimmerLink: "Camere",
   GallerieLink: "Gelerie",
-  AlertText1:"Această cameră nu este momentan disponibilă online.",
-  AlertText2:"Întrebări și rezervări de camere la:",
-  ZurückButton:"Înapoi"
+  AlertText1: "Această cameră nu este momentan disponibilă online.",
+  AlertText2: "Întrebări și rezervări de camere la:",
+  ZurückButton: "Înapoi",
 };
 
 export const spanisch = {
   Feld1: "Personas:",
-  Feld2: 2 - 4,
+  Feld2: "2-4",
   Feld3: "Habitación:",
   Feld4: 2,
   Feld5: "Cocina:",
@@ -121,7 +123,8 @@ export const spanisch = {
   Feld12: "desde 40 € / noche",
   Feld12APPII: "desde 35 € / noche",
   Feld12Garsoniera: "desde 22€ / noche",
-  Feld13:"Ofertas especiales para inquilinos a largo plazo y descuentos bajo petición.",
+  Feld13:
+    "Ofertas especiales para inquilinos a largo plazo y descuentos bajo petición.",
   AirBnbButton: "Consulta disponibilidad y reserva aquí",
   TextHeadline: "Bienvenidos a Transilvania",
   Text1:
@@ -136,13 +139,13 @@ export const spanisch = {
   HomeLink: "Home",
   ZimmerLink: "Habitaciones",
   GallerieLink: "Galería",
-  AlertText1:"Actualmente, esta sala no está disponible en línea.",
-  AlertText2:"Consultas y reservas de habitaciones en:",
-  ZurückButton:"Atrás"
+  AlertText1: "Actualmente, esta sala no está disponible en línea.",
+  AlertText2: "Consultas y reservas de habitaciones en:",
+  ZurückButton: "Atrás",
 };
 export const französisch = {
   Feld1: "Personnes:",
-  Feld2: 2 - 4,
+  Feld2: "2-4",
   Feld3: "Chambre:",
   Feld4: 2,
   Feld5: "Cuisine:",
@@ -156,7 +159,8 @@ export const französisch = {
   Feld12: "à partir de 40 € / nuit",
   Feld12Garsoniera: "à partir de 22 € / nuit",
   Feld12APPII: "à partir de 35 € / nuit",
-  Feld13:"Offres spéciales pour les locataires à long terme et réductions sur demande.",
+  Feld13:
+    "Offres spéciales pour les locataires à long terme et réductions sur demande.",
   AirBnbButton: "Vérifiez la disponibilité et réservez ici",
   TextHeadline: "Bienvenue en Transylvanie",
   Text1:
@@ -171,14 +175,14 @@ export const französisch = {
   HomeLink: "Home",
   ZimmerLink: "Chambres",
   GallerieLink: "Galerie",
-  AlertText1:"Cette chambre n'est actuellement pas disponible en ligne.",
-  AlertText2:"Demandes de renseignements et réservations de chambres à:",
-  ZurückButton:"Retour"
+  AlertText1: "Cette chambre n'est actuellement pas disponible en ligne.",
+  AlertText2: "Demandes de renseignements et réservations de chambres à:",
+  ZurückButton: "Retour",
 };
 
 export const ungarisch = {
   Feld1: "Emberek:",
-  Feld2: 2 - 4,
+  Feld2: "2-4",
   Feld3: "Szoba:",
   Feld4: 2,
   Feld5: "Konyha:",
@@ -192,7 +196,8 @@ export const ungarisch = {
   Feld12: "40 € / éj-től",
   Feld12Garsoniera: "22 € / éj-től",
   Feld12APPII: "35 € / éj-től",
-  Feld13:"Különleges ajánlatok hosszú távú bérlők számára, és kérésre kedvezmények.",
+  Feld13:
+    "Különleges ajánlatok hosszú távú bérlők számára, és kérésre kedvezmények.",
   AirBnbButton: "Ellenőrizze a rendelkezésre állást és foglaljon itt",
   TextHeadline: "Üdvözöljük Erdélyben",
   Text1:
@@ -207,14 +212,14 @@ export const ungarisch = {
   HomeLink: "Home",
   ZimmerLink: "Szoba",
   GallerieLink: "Képtár",
-  AlertText1:"Ez a szoba jelenleg nem érhető el online.",
-  AlertText2:"Érdeklődések és szobafoglalások itt:",
-  ZurückButton:"Vissza"
+  AlertText1: "Ez a szoba jelenleg nem érhető el online.",
+  AlertText2: "Érdeklődések és szobafoglalások itt:",
+  ZurückButton: "Vissza",
 };
 
 export const hebräisch = {
   Feld1: "אֲנָשִׁים:",
-  Feld2: 2 - 4,
+  Feld2: "2-4",
   Feld3: "חֶדֶר:",
   Feld4: 2,
   Feld5: "מִטְבָּח:",
@@ -228,7 +233,7 @@ export const hebräisch = {
   Feld12: "מ 40 € / לילה",
   Feld12Garsoniera: "מ 25 € / לילה",
   Feld12APPII: "מ 35 € / לילה",
-  Feld13:"מבצעים מיוחדים לשוכרים לטווח ארוך והנחות על פי בקשה.",
+  Feld13: "מבצעים מיוחדים לשוכרים לטווח ארוך והנחות על פי בקשה.",
   AirBnbButton: "בדקו זמינות והזמינו כאן",
   TextHeadline: "ברוך הבא לטרנסילבניה",
   Text1: "אנו מקבלים את פניכם בדירות ובחדרי האירוח שלנו בלב רומניה.",
@@ -242,7 +247,264 @@ export const hebräisch = {
   HomeLink: "בית",
   ZimmerLink: "חדרים",
   GallerieLink: "גלריה",
-  AlertText1:"חדר זה אינו זמין כעת באינטרנט.",
-  AlertText2:"פניות והזמנות חדרים ב:",
-  ZurückButton:"חזור"
+  AlertText1: "חדר זה אינו זמין כעת באינטרנט.",
+  AlertText2: "פניות והזמנות חדרים ב:",
+  ZurückButton: "חזור",
 };
+
+const languages = {
+  deutsch: {
+    Feld1: "Personen:",
+    Feld2: '"2-4"',
+    Feld3: "Zimmer:",
+    Feld4: 2,
+    Feld5: "Küchenzeile:",
+    Feld6: "Ja. (Ohne Herdplatte)",
+    Feld6Garsoniera: "Nein. Wasserkocher und Kaffemaschine vorhanden",
+    Feld7: "Badezimmer:",
+    Feld8: "Privat",
+    Feld9: "Netflix:",
+    Feld10: "✔️",
+    Feld11: "Preis:",
+    Feld12: "ab 40€ / Nacht",
+    Feld12Garsoniera: "ab 22€ / Nacht",
+    Feld12APPII: "ab 35€ / Nacht",
+    Feld13:
+      "Spezielle Angebote für Langzeitmieter und Vergünstigungen auf Anfrage.",
+    AirBnbButton: "Hier Verfügbarkeit überprüfen und buchen",
+    TextHeadline: "Willkommen in Siebenbürgen.",
+    Text1:
+      "Wir begrüßen Sie in unseren Ferienwohnungen und Fremdenzimmern im Herzen Rumäniens.",
+    Text2:
+      "Aus unserer Unterkunft gelangen Sie schnell ins Stadtzentrum von Kronstadt/Braşov, wo Sie zahlreiche Einkaufsmöglichkeiten und Gastronomien mit landestypischen Spezialitäten erwarten.",
+    Text3:
+      "Von Kronstadt aus gelangen Sie schnell zu den höchsten Spitzen der Karpaten, die waldreichste Gegend Europas, für welche das Land bekannt ist.",
+    Text4: "Ein super Platz für alle Wanderfreunde.",
+    Text5:
+      "Im Winter läd sie unter anderem das anliegende Skigebiet zum Skifahren, oder zu einem wärmenden Glühschnaps ein.",
+    HomeLink: "Home",
+    ZimmerLink: "Zimmer",
+    GallerieLink: "Gallerie",
+    AlertText1: "Dieses Zimmer ist online momentan nicht verfügbar.",
+    AlertText2: "Anfragen & Zimmerbuchungen unter:",
+    ZurückButton: "Zurück",
+  },
+
+  englisch: {
+    Feld1: "Persons:",
+    Feld2: "2-4",
+    Feld3: "Rooms:",
+    Feld4: 2,
+    Feld5: "Kitchen:",
+    Feld6: "Yes. (Without oven)",
+    Feld6Garsoniera: "No. Kettle and coffee machine available",
+    Feld7: "Bathroom:",
+    Feld8: "Private",
+    Feld9: "Netflix:",
+    Feld10: "✔️",
+    Feld11: "Price:",
+    Feld12: "from 40€ / night",
+    Feld12Garsoniera: "from 22€ / night",
+    Feld12APPII: "from 35€ / night",
+    Feld13: "Special offers for long-term rent and discounts on request.",
+    AirBnbButton: "Check availability and book here",
+    TextHeadline: "Welcome to Transylvania.",
+    Text1:
+      "We welcome you to our apartments and guest rooms in the heart of Romania.",
+    Text2:
+      "From our accommodation you can quickly reach the city center of Kronstadt / Braşov, where you will find numerous shopping opportunities and restaurants with local specialties.",
+    Text3:
+      "From Kronstadt you can quickly reach the highest peaks of the Carpathian Mountains, the most wooded area in Europe for which the country is known.",
+    Text4: "A great place for all hikers.",
+    Text5:
+      "In winter, among other things, the georgeous ski area invites you to go skiing or to enjoy a warming glow schnapps.",
+    HomeLink: "Home",
+    ZimmerLink: "Rooms",
+    GallerieLink: "Gallery",
+    AlertText1: "This room is currently unavailable online.",
+    AlertText2: "Please call the number for inquiries & room bookings:",
+    ZurückButton: "Back",
+  },
+
+  rumänisch: {
+    Feld1: "Persoane:",
+    Feld2: "2-4",
+    Feld3: "Cameră:",
+    Feld4: 2,
+    Feld5: "Bucătărie:",
+    Feld6: "Da (fără aragaz)",
+    Feld6Garsoniera: "Nu. Ceainic și aparat de cafea sunt disponibile",
+    Feld7: "Baie:",
+    Feld8: "Privat",
+    Feld9: "Netflix:",
+    Feld10: "✔️",
+    Feld11: "Preț:",
+    Feld12: "de la 40 € / noapte",
+    Feld12Garsoniera: "de la 22 € / noapte",
+    Feld12APPII: "de la 35 € / noapte",
+    Feld13:
+      "Oferte speciale pentru chiriașii pe termen lung și reduceri la cerere.",
+    AirBnbButton: "Verificați disponibilitatea și rezervați aici",
+    TextHeadline: "Bine ați venit la Brașov.",
+    Text1:
+      "Vă așteptăm în apartamentele și camerele noastre din inima României.",
+    Text2:
+      "De la cazare, puteți ajunge rapid în centrul orașului Kronstadt / Brașov, unde veți găsi numeroase oportunități de cumpărături și restaurante cu specialități tipice.",
+    Text3:
+      "De la Brașov puteți ajunge rapid pe cele mai înalte vârfuri ale Munților Carpați, cea mai împădurită zonă din Europa pentru care țara este cunoscută.",
+    Text4: "Un loc minunat pentru toți excursioniștii.",
+    Text5:
+      "Iarna, printre altele, zona de schi adiacentă vă invită să mergeți la schi sau să vă bucurați de o schnapps strălucitoare.",
+    HomeLink: "Home",
+    ZimmerLink: "Camere",
+    GallerieLink: "Gelerie",
+    AlertText1: "Această cameră nu este momentan disponibilă online.",
+    AlertText2: "Întrebări și rezervări de camere la:",
+    ZurückButton: "Înapoi",
+  },
+
+  spanisch: {
+    Feld1: "Personas:",
+    Feld2: "2-4",
+    Feld3: "Habitación:",
+    Feld4: 2,
+    Feld5: "Cocina:",
+    Feld6: "Sí (sin estufa)",
+    Feld6Garsoniera: "No. Hervidor y cafetera disponibles",
+    Feld7: "Baño:",
+    Feld8: "Privado",
+    Feld9: "Netflix:",
+    Feld10: "✔️",
+    Feld11: "Precio:",
+    Feld12: "desde 40 € / noche",
+    Feld12APPII: "desde 35 € / noche",
+    Feld12Garsoniera: "desde 22€ / noche",
+    Feld13:
+      "Ofertas especiales para inquilinos a largo plazo y descuentos bajo petición.",
+    AirBnbButton: "Consulta disponibilidad y reserva aquí",
+    TextHeadline: "Bienvenidos a Transilvania",
+    Text1:
+      "Le damos la bienvenida a nuestros apartamentos y habitaciones en el corazón de Rumanía.",
+    Text2:
+      "Desde nuestro alojamiento puede llegar rápidamente al centro de la ciudad de Kronstadt / Braşov, donde encontrará numerosas oportunidades de compras y restaurantes con especialidades locales.",
+    Text3:
+      "Desde Kronstadt se puede llegar rápidamente a los picos más altos de los Cárpatos, la zona más boscosa de Europa por la que se conoce al país.",
+    Text4: "Un gran lugar para todos los excursionistas.",
+    Text5:
+      "En invierno, entre otras cosas, la zona de esquí adyacente le invita a esquiar o disfrutar de un cálido aguardiente.",
+    HomeLink: "Home",
+    ZimmerLink: "Habitaciones",
+    GallerieLink: "Galería",
+    AlertText1: "Actualmente, esta sala no está disponible en línea.",
+    AlertText2: "Consultas y reservas de habitaciones en:",
+    ZurückButton: "Atrás",
+  },
+  französisch: {
+    Feld1: "Personnes:",
+    Feld2: "2-4",
+    Feld3: "Chambre:",
+    Feld4: 2,
+    Feld5: "Cuisine:",
+    Feld6: "Oui (sans cuisinière)",
+    Feld6Garsoniera: "Non. Bouilloire et machine à café disponibles",
+    Feld7: "Salle de bain:",
+    Feld8: "Privée",
+    Feld9: "Netflix:",
+    Feld10: "✔️",
+    Feld11: "Prix:",
+    Feld12: "à partir de 40 € / nuit",
+    Feld12Garsoniera: "à partir de 22 € / nuit",
+    Feld12APPII: "à partir de 35 € / nuit",
+    Feld13:
+      "Offres spéciales pour les locataires à long terme et réductions sur demande.",
+    AirBnbButton: "Vérifiez la disponibilité et réservez ici",
+    TextHeadline: "Bienvenue en Transylvanie",
+    Text1:
+      "Nous vous accueillons dans nos appartements et chambres d'hôtes au coeur de la Roumanie.",
+    Text2:
+      "Depuis notre hébergement, vous pouvez rejoindre rapidement le centre-ville de Kronstadt / Braşov, où vous trouverez de nombreux commerces et restaurants proposant des spécialités locales typiques.",
+    Text3:
+      "De Brasov, vous pouvez rapidement atteindre les plus hauts sommets des Carpates, la région la plus boisée d'Europe pour laquelle le pays est connu.",
+    Text4: "Un endroit idéal pour tous les randonneurs.",
+    Text5:
+      "En hiver, entre autres, le domaine skiable adjacent vous invite à faire du ski ou à profiter d'un schnaps réchauffant.",
+    HomeLink: "Home",
+    ZimmerLink: "Chambres",
+    GallerieLink: "Galerie",
+    AlertText1: "Cette chambre n'est actuellement pas disponible en ligne.",
+    AlertText2: "Demandes de renseignements et réservations de chambres à:",
+    ZurückButton: "Retour",
+  },
+
+  ungarisch: {
+    Feld1: "Emberek:",
+    Feld2: "2-4",
+    Feld3: "Szoba:",
+    Feld4: 2,
+    Feld5: "Konyha:",
+    Feld6: "Igen (tűzhely nélkül)",
+    Feld6Garsoniera: "Nem. Vízforraló és kávéfőző áll rendelkezésre",
+    Feld7: "Fürdőszoba:",
+    Feld8: "Magán",
+    Feld9: "Netflix:",
+    Feld10: "✔️",
+    Feld11: "Ár:",
+    Feld12: "40 € / éj-től",
+    Feld12Garsoniera: "22 € / éj-től",
+    Feld12APPII: "35 € / éj-től",
+    Feld13:
+      "Különleges ajánlatok hosszú távú bérlők számára, és kérésre kedvezmények.",
+    AirBnbButton: "Ellenőrizze a rendelkezésre állást és foglaljon itt",
+    TextHeadline: "Üdvözöljük Erdélyben",
+    Text1:
+      "Szeretettel várjuk Önt Románia szívében található apartmanjainkba és vendégszobáinkba.",
+    Text2:
+      "Szálláshelyünkről gyorsan eljuthat Kronstadt / Brassó városközpontjába, ahol számos vásárlási lehetőségre és helyi specialitásokkal rendelkező étterembe számíthat.",
+    Text3:
+      "Kronstadtból gyorsan elérheti a Kárpátok legmagasabb csúcsait, Európa legerdősebb területét, amelyről az ország ismert.",
+    Text4: "Remek hely minden túrázó számára.",
+    Text5:
+      "Télen többek között a szomszédos síterület meghívja Önt síelni vagy egy melegítő pálinkát elfogyasztani.",
+    HomeLink: "Home",
+    ZimmerLink: "Szoba",
+    GallerieLink: "Képtár",
+    AlertText1: "Ez a szoba jelenleg nem érhető el online.",
+    AlertText2: "Érdeklődések és szobafoglalások itt:",
+    ZurückButton: "Vissza",
+  },
+  hebräisch: {
+    Feld1: "אֲנָשִׁים:",
+    Feld2: "2-4",
+    Feld3: "חֶדֶר:",
+    Feld4: 2,
+    Feld5: "מִטְבָּח:",
+    Feld6: "(כן (ללא כיריים",
+    Feld6Garsoniera: "לא. קומקום ומכונת קפה זמינים",
+    Feld7: "חדר אמבטיה:",
+    Feld8: "פְּרָטִי",
+    Feld9: "Netflix:",
+    Feld10: "✔️",
+    Feld11: "מחיר:",
+    Feld12: "מ 40 € / לילה",
+    Feld12Garsoniera: "מ 25 € / לילה",
+    Feld12APPII: "מ 35 € / לילה",
+    Feld13: "מבצעים מיוחדים לשוכרים לטווח ארוך והנחות על פי בקשה.",
+    AirBnbButton: "בדקו זמינות והזמינו כאן",
+    TextHeadline: "ברוך הבא לטרנסילבניה",
+    Text1: "אנו מקבלים את פניכם בדירות ובחדרי האירוח שלנו בלב רומניה.",
+    Text2:
+      "ממקום האירוח תוכלו להגיע במהירות למרכז העיר קרונשטאט / בראשוב, שם תוכלו למצוא אפשרויות רבות לקניות ומסעדות עם התמחויות טיפוסיות.",
+    Text3:
+      "מקונשטאדט תוכלו להגיע במהירות לפסגות הגבוהות ביותר של הרי הקרפטים, האזור המיוער ביותר באירופה שהמדינה ידועה בזכותו.",
+    Text4: "מקום נהדר לכל המטיילים.",
+    Text5:
+      "בחורף, בין היתר, אזור הסקי הצמוד מזמין אתכם לעשות סקי או ליהנות משנאפס זוהר מחמם.",
+    HomeLink: "בית",
+    ZimmerLink: "חדרים",
+    GallerieLink: "גלריה",
+    AlertText1: "חדר זה אינו זמין כעת באינטרנט.",
+    AlertText2: "פניות והזמנות חדרים ב:",
+    ZurückButton: "חזור",
+  },
+};
+export default languages;
